e6135ac740417c1150a4e7a9334854a990af8d84
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-03-23 02:31:28 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,16 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+  end
+  
+  def brand
+    @brand
+    BRANDS << BRANDS
   end
 
   def cobble
